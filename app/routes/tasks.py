ced34ef2dd90adbe2642ba3172aef2e749bc84ef
NagesoHaile <nageehail12@gmail.com>
2025-04-21 02:13:36 +0300

    feat:Task--> create_task,get_task_by_id, get_all_tasks  completed.

--- app/routes/tasks.py
+++ app/routes/tasks.py
@@ -1,24 +1,32 @@
-from fastapi import APIRouter,Depends, HTTPException,status
+from fastapi import APIRouter,Depends, HTTPException,status,Request
 from sqlmodel import Session,select
 from typing import List
 from app.models.task import Task
-from app.schemas.task import TaskCreate,TaskRead
+from app.schemas.task import TaskCreate,TaskListResponse
 from app.config.database import get_session
 from app.middleware.auth import JWTBearer
+import app.services.task_services as TaskService
 
-router = APIRouter(prefix='/tasks',tags=['Tasks'])
+router = APIRouter(prefix='/tasks',tags=['Tasks'],dependencies=[Depends(JWTBearer())])
 
-@router.get('',response_model=List[TaskRead],dependencies=[Depends(JWTBearer())])
+@router.get('',response_model=TaskListResponse)
 def get_tasks(db:Session = Depends(get_session)):
-    command = select(Task)
-    tasks = db.exec(command).all()
-    return tasks
+     tasks =  TaskService.get_all_tasks(db=db)
+     return {"ok":True,"data":tasks}
+    
 
 @router.post('',response_model=TaskCreate,status_code=status.HTTP_201_CREATED)
-def create_task(task:TaskCreate,db:Session = Depends(get_session)):
+def create_task(request:Request,task:TaskCreate,db:Session = Depends(get_session)):
+    user_id = request.state.user.id
+    task.user_id = user_id
     new_task = Task.model_validate(task)
-    db.add(new_task)
-    db.commit()
-    db.refresh(new_task)
-    return new_task
+    return TaskService.create_task(db=db,task=new_task)
+
+
+@router.get('/{task_id}')
+def get_task_by_id(task_id:int,db:Session = Depends(get_session)):
+    task = TaskService.get_task_by_id(db=db,task_id=task_id)
+    return task
+
+
     
--- app/services/task_services.py
+++ app/services/task_services.py
@@ -1,36 +1,58 @@
 from sqlmodel import Session,select
 from app.models.task import Task
 from typing import List,Optional
+from fastapi import HTTPException
+from app.schemas.task import TaskRead
+from app.schemas.response import SuccessResponse
 
-def create_taks(db:Session,task:Task)-> Task:
+def create_task(db:Session,task:Task)-> Task:
+    """
+        It takes db:Session and
+        task:Task
+        returns Task
+    """
     db.add(task)
     db.commit()
     db.refresh(task)
     return task
 
 
-def get_task_by_id(db:Session,task_id:int)-> Optional[Task]:
-    return db.get(Task,task_id)
+def get_task_by_id(db:Session,task_id:int)-> SuccessResponse:
+    """
+    Fetch a task by ID, wrap it in SuccessResponse.
+    """
+    try:
+        task = db.exec(select(Task).where(Task.id == int(task_id)))
+        if task is None:
+            raise HTTPException(status_code=404,detail="Task not found")
+        return SuccessResponse(data=task)
+    except Exception as e:
+        raise HTTPException(status_code=500,detail="Internal Server Error")
 
-def get_all_tasks(db:Session)->List[Task]:
+
+
+
+def get_all_tasks(db:Session):
     return db.exec(select(Task)).all()
 
-"""
+
+def update_task(db:Session,task:Task)->Task:
+    """
 It takes [session] and task of Type Task
 and returns Task
 """
-def update_task(db:Session,task:Task)->Task:
     db.add(task)  # in this one SQLModel can track changes
     db.commit()
     db.refresh(task)
     return task
 
-"""
+
+def delete_task(db:Session,task_id:int)->bool:
+    """
 function to delete a task with given [task_id]
 and it returns boolean value based on the
 database operations result.
 """
-def delete_task(db:Session,task_id:int)->bool:
     task = db.get(Task,task_id)
     if task:
         db.delete(task)
