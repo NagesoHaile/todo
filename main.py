d11b5f372e9ba800ff2aa0c800c209ef7742e5d5
NagesoHaile <nageehail12@gmail.com>
2025-04-21 00:49:33 +0300

    feat:  SETUP -> auth routes and task routes registration and application startup.

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from fastapi import FastAPI,Depends
+from app.config.database import init_db
+from app.routes.tasks import router as task_router
+from app.routes.auth_routes import router as auth_router
+from app.middleware.auth import JWTBearer
+@asynccontextmanager
+async def lifespan(ap:FastAPI):
+    init_db()
+    yield
 
-app = FastAPI()
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(task_router,prefix="/api",tags=['Tasks'])
+# app.include_router(task_router,prefix="/api",tags=['Tasks'])
+app.include_router(auth_router,prefix="/api/auth",tags=["Authentication"])
 
 @app.get("/")
 def read_root():
