d9dcbbe565cd1e779badfbd207be161157521e04
NagesoHaile <nageehail12@gmail.com>
2025-04-21 02:11:51 +0300

    feat: Task --> response orchestrator for TaskListResponse schema

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -1,31 +1,35 @@
-from typing import Optional
+from typing import Optional,List
 from datetime import datetime
 from pydantic import BaseModel
 
 class TaskBase(BaseModel):
     title:str
     description: Optional[str] = None
-    is_completed: bool = False
+    is_completed: Optional[bool] = False
 
 # for creating tasks
-"""
+
+class TaskCreate(TaskBase):
+    """
  Schema used for creating a task
  
 """
-class TaskCreate(TaskBase):
-    pass 
+    user_id:Optional[int] = None 
 
-"""
- schema used to read a task
- DB -> client
-"""
+
 class TaskRead(TaskBase):
+    """
+        schema used to read a task
+        DB -> client
+    """
     id: int
-    created_at:datetime
-    updated_at:datetime
-    owner_id: Optional[int]
+    user_id: Optional[int]
 
 
     class Config:
         orm_mode = True
 
+
+class TaskListResponse(BaseModel):
+    ok:bool = True
+    data:List[TaskRead] = None
